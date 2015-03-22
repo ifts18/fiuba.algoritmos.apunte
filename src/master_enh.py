8e05dd87538a6f3d01f3aee861fe11960250ad87
Margarita Manterola <marga@debian.org>
2015-03-22 18:10:55 +0100

    Arreglos en la seccion de cadenas de formato y en el mastermind

--- src/master_enh.py
+++ src/master_enh.py
@@ -10,38 +10,37 @@ digitos = ('0','1','2','3','4','5','6','7','8','9')
 cant_digitos = 5
 codigo = ''
 for i in range(cant_digitos):
+    candidato = random.choice(digitos)
+    # vamos eligiendo digitos no repetidos
+    while candidato in codigo:
         candidato = random.choice(digitos)
-        # vamos eligiendo digitos no repetidos
-        while candidato in codigo:
-                candidato = random.choice(digitos)
-        codigo = codigo + candidato
+    codigo = codigo + candidato
 
 # iniciamos interaccion con el usuario
 print "Bienvenido/a al Mastermind!"
-print "Tenes que adivinar un numero de", cant_digitos, \
-	"cifras distintas"
+print "Tenes que adivinar un numero de %d cifras distintas" % cant_digitos
 propuesta = raw_input("Que codigo propones?: ")
 
 # procesamos las propuestas e indicamos aciertos y coincidencias
 intentos = 1
 while propuesta != codigo and propuesta != "Me doy":
-	intentos = intentos + 1
-        aciertos = 0
-        coincidencias = 0
+    intentos = intentos + 1
+    aciertos = 0
+    coincidencias = 0
 
-        # recorremos la propuesta y verificamos en el codigo
-        for i in range(cant_digitos):
-                if propuesta[i] == codigo[i]:
-                        aciertos = aciertos + 1
-                elif propuesta[i] in codigo:
-                        coincidencias = coincidencias + 1
-        print "Tu propuesta (", propuesta, ") tiene", aciertos, \
-		"aciertos y ", coincidencias, "coincidencias."
-        # pedimos siguiente propuesta
-        propuesta = raw_input("Propone otro codigo: ")
+    # recorremos la propuesta y verificamos en el codigo
+    for i in range(cant_digitos):
+        if propuesta[i] == codigo[i]:
+            aciertos = aciertos + 1
+        elif propuesta[i] in codigo:
+            coincidencias = coincidencias + 1
+    print "Tu propuesta (%s) tiene %d aciertos y %d coincidencias." % \
+        (propuesta, aciertos, coincidencias)
+    # pedimos siguiente propuesta
+    propuesta = raw_input("Propone otro codigo: ")
+
 if propuesta == "Me doy":
-	print "El codigo era", codigo
-	print "Suerte la proxima vez!"
+    print "El codigo era: %s" % codigo
+    print "Suerte la proxima vez!"
 else:
-	print "Felicitaciones! Adivinaste el codigo en", \
-		intentos, "intentos."
+    print "Felicitaciones! Adivinaste el codigo en %d intentos." % intentos
--- src/mastermind.py
+++ src/mastermind.py
@@ -9,33 +9,33 @@ digitos = ('0','1','2','3','4','5','6','7','8','9')
 # "elegimos" el codigo
 codigo = ''
 for i in range(4):
+    candidato = random.choice(digitos)
+    # vamos eligiendo digitos no repetidos
+    while candidato in codigo:
         candidato = random.choice(digitos)
-        # vamos eligiendo digitos no repetidos
-        while candidato in codigo:
-                candidato = random.choice(digitos)
-        codigo = codigo + candidato
+    codigo = codigo + candidato
 
 # iniciamos interaccion con el usuario
 print "Bienvenido/a al Mastermind!"
-print "Tenes que adivinar un numero de", 4, "cifras distintas"
+print "Tenes que adivinar un numero de cuatro cifras distintas"
 propuesta = raw_input("Que codigo propones?: ")
 
 # procesamos las propuestas e indicamos aciertos y coincidencias
 intentos = 1
 while propuesta != codigo:
-	intentos = intentos + 1
-        aciertos = 0
-        coincidencias = 0
+    intentos = intentos + 1
+    aciertos = 0
+    coincidencias = 0
 
         # recorremos la propuesta y verificamos en el codigo
-        for i in range(4):
-                if propuesta[i] == codigo[i]:
-                        aciertos = aciertos + 1
-                elif propuesta[i] in codigo:
-                        coincidencias = coincidencias + 1
-        print "Tu propuesta (", propuesta, ") tiene", aciertos, \
-		"aciertos y ", coincidencias, "coincidencias."
-        # pedimos siguiente propuesta
-        propuesta = raw_input("Propone otro codigo: ")
+    for i in range(4):
+        if propuesta[i] == codigo[i]:
+            aciertos = aciertos + 1
+        elif propuesta[i] in codigo:
+            coincidencias = coincidencias + 1
+    print "Tu propuesta (%s) tiene %d aciertos y %d coincidencias." % \
+        (propuesta, aciertos, coincidencias)
+    # pedimos siguiente propuesta
+    propuesta = raw_input("Propone otro codigo: ")
 
-print "Felicitaciones! Adivinaste el codigo en", intentos, "intentos."
+print "Felicitaciones! Adivinaste el codigo en %d intentos." % intentos
