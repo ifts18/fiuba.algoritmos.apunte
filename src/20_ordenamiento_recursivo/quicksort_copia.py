42b7b57d9b3277c041fe371881d39e2f0905e143
Diego Essaya <dessaya@gmail.com>
2016-02-14 12:28:54 -0300

    Ajustes al capítulo 20

--- src/20_ordenamiento_recursivo/quicksort_copia.py
+++ src/20_ordenamiento_recursivo/quicksort_copia.py
@@ -1,26 +1,19 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
 def quick_sort(lista):
-    """ Ordena la lista de forma recursiva.
-        Pre: los elementos de la lista deben ser comparables.
-        Devuelve: una nueva lista con los elementos ordenados. """
-
-    # Caso base
+    """Ordena la lista de forma recursiva.
+       Pre: los elementos de la lista deben ser comparables.
+       Devuelve: una nueva lista con los elementos ordenados."""
     if len(lista) < 2:
         return lista
-    # Caso recursivo
     menores, medio, mayores = _partition(lista)
     return quick_sort(menores) + medio + quick_sort(mayores)
 
 def _partition(lista):
-    """ Pre: lista no vacía.
-        Devuelve: tres listas: menores, medio y mayores. """
-
+    """Pre: lista no vacía.
+       Devuelve: tres listas: menores, medio y mayores."""
     pivote = lista[0]
     menores = []
     mayores = []
-    for x in xrange(1, len(lista)):
+    for x in range(1, len(lista)):
         if lista[x] < pivote:
             menores.append(lista[x])
         else:
